c7df7aaa0682f27ab05faf75e2ae7a5e08ac7d13
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2024-06-03 16:38:28 +0200

    add file

--- Indian Diabetes.py
+++ Indian Diabetes.py
@@ -23,21 +23,25 @@ df.head()
 X = df.loc[:, df.columns != 'DiabetesPresence']
 Y = df.loc[:, 'DiabetesPresence']
 
+# Define the model
 model = Sequential()
 model.add(Dense(12, input_shape=(8,), activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(8, activation='relu')) # essai couche en plus
 model.add(Dense(8, activation='relu')) # essai couche en plus
-
 model.add(Dense(1, activation='sigmoid'))
 
+# Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Train the model
 model.fit(X, Y, epochs=100)
 
+# Evaluate the model on the entire dataset
 _, accuracy = model.evaluate(X, Y)
 print('Accuracy: %.2f' % (accuracy*100))
 
+# Split the data into training and test sets
 df_train = df.sample(frac=0.8)
 df_test = df.drop(df_train.index)
 
@@ -48,12 +52,16 @@ Y_train = df_train.loc[:, 'DiabetesPresence']
 X_test = df_test.loc[:, df.columns != 'DiabetesPresence']
 Y_test = df_test.loc[:, 'DiabetesPresence']
 
+# Plot the model
 plot_model(model, to_file="model.png", show_shapes=True, show_layer_names=False, show_layer_activations=True)
 
+# Re-compile the model before training
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Train the model with validation split
 history = model.fit(X_train, Y_train, validation_split=0.2, epochs=50, batch_size=10)
 
+# Plot the training and validation accuracy
 plt.plot(history.history['accuracy'], color='#066b8b')
 plt.plot(history.history['val_accuracy'], color='#b39200')
 plt.title('model accuracy')
@@ -62,6 +70,7 @@ plt.xlabel('epoch')
 plt.legend(['train', 'val'], loc='upper left')
 plt.show()
 
+# Predict on the test set
 predictions = model.predict(X_test)
 
 predictions[0]
@@ -69,6 +78,7 @@ predictions = (model.predict(X_test) > 0.5).astype(int)
 for i in range(5):
 	print('%s => Prédit : %d,  Attendu : %d' % (X_test.iloc[i].tolist(), predictions[i], Y_test.iloc[i]))
     
+# Evaluate the model on the test set
 _, accuracy = model.evaluate(X_test, Y_test)
 print('Accuracy: %.2f' % (accuracy*100))
 
